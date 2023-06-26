4334198d861ae91811cba5aac73146b38475525c
Chileap Chhin <chileapchhin@gmail.com>
2023-06-26 14:27:05 +0700

    fixed rspec

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :view do
-  it "displays the welcome message" do
-    render
-    expect(rendered).to include("Get started")
+
+  describe "GET /" do
+    let(:user) { create(:user) }
+
+    before do
+      allow(view).to receive(:current_user).and_return(user)
+    end
+
+    it "displays the welcome message" do
+      render
+      expect(rendered).to include("Get started")
+    end
   end
 end
